d7626ec0a08d659b6da96a35dce7dfac41f37900
faisalakb <faisalakb786@gmail.com>
2023-08-11 01:04:36 +0500

    add events for numbers & operation

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Operator from './Operator';
+import calculation from '../logic/calculate';
 
-const Calculator = () => (
+const Calculator = () => {
+  const [objData, setObj] = useState({
+    total: '',
+    next: '',
+    operation: '',
+  });
 
-  <div className="calculator">
-    <input type="text" dir="rtl" className="inp" />
-    <Operator className="operationContainer" />
-    <div className="numbers">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
+  const setValue = (btnName) => {
+    const setdata = calculation(objData, btnName);
+    setObj(setdata);
+  };
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setObj({ ...objData, next: inputValue });
+  };
+
+  const DisplayInp = () => (
+    <>
+      <input
+        id="inpID"
+        type="text"
+        value={`${objData.total || ''}${objData.operation || ''}${objData.next || ''}`}
+        onChange={handleInputChange}
+        className="inp"
+      />
+    </>
+  );
+
+  return (
+
+    <div className="calculator">
+      <DisplayInp />
+      <Operator className="operationContainer" handleClick={setValue} />
+      <div className="numbers">
+        <button type="button" onClick={() => setValue('7')}>7</button>
+        <button type="button" onClick={() => setValue('8')}>8</button>
+        <button type="button" onClick={() => setValue('9')}>9</button>
+        <button type="button" onClick={() => setValue('4')}>4</button>
+        <button type="button" onClick={() => setValue('5')}>5</button>
+        <button type="button" onClick={() => setValue('6')}>6</button>
+        <button type="button" onClick={() => setValue('1')}>1</button>
+        <button type="button" onClick={() => setValue('2')}>2</button>
+        <button type="button" onClick={() => setValue('3')}>3</button>
+      </div>
+      <div className="zeroDiv">
+        <button type="button" onClick={() => setValue('0')} className="zero">0</button>
+        <button type="button" onClick={() => setValue('.')}>.</button>
+      </div>
     </div>
-    <div className="zeroDiv">
-      <button type="button" className="zero">0</button>
-      <button type="button">.</button>
-    </div>
-  </div>
-
-);
+  );
+};
 
 export default Calculator;
--- src/components/Operator.jsx
+++ src/components/Operator.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Operator = () => (
+const Operator = ({ handleClick }) => (
   <>
     <div className="operation">
-      <button type="button">÷</button>
-      <button type="button">x</button>
-      <button type="button">-</button>
-      <button type="button">+</button>
-      <button type="button">=</button>
+      <button type="button" onClick={() => handleClick('÷')}>÷</button>
+      <button type="button" onClick={() => handleClick('x')}>x</button>
+      <button type="button" onClick={() => handleClick('-')}>-</button>
+      <button type="button" onClick={() => handleClick('+')}>+</button>
+      <button type="button" onClick={() => handleClick('=')}>=</button>
     </div>
     <div className="top">
-      <button type="button" className="clear">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
+      <button type="button" onClick={() => handleClick('AC')} className="clear">AC</button>
+      <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
+      <button type="button" onClick={() => handleClick('%')}>%</button>
     </div>
   </>
 );
+
+Operator.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Operator;
