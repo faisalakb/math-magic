1359680df2bc31ba5069e86bf8e240936bd62b1e
faisalakb <faisalakb786@gmail.com>
2023-08-11 16:11:33 +0500

    Use API to fetch data

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Operator from './Operator';
 import calculation from '../logic/calculate';
+import DisplayApi from './displayApi';
 
 const Calculator = () => {
   const [objData, setObj] = useState({
@@ -33,25 +34,29 @@ const Calculator = () => {
 
   return (
 
-    <div className="calculator">
-      <DisplayInp />
-      <Operator className="operationContainer" handleClick={setValue} />
-      <div className="numbers">
-        <button type="button" onClick={() => setValue('7')}>7</button>
-        <button type="button" onClick={() => setValue('8')}>8</button>
-        <button type="button" onClick={() => setValue('9')}>9</button>
-        <button type="button" onClick={() => setValue('4')}>4</button>
-        <button type="button" onClick={() => setValue('5')}>5</button>
-        <button type="button" onClick={() => setValue('6')}>6</button>
-        <button type="button" onClick={() => setValue('1')}>1</button>
-        <button type="button" onClick={() => setValue('2')}>2</button>
-        <button type="button" onClick={() => setValue('3')}>3</button>
+    <>
+      <div className="calculator">
+        <DisplayInp />
+        <Operator className="operationContainer" handleClick={setValue} />
+        <div className="numbers">
+          <button type="button" onClick={() => setValue('7')}>7</button>
+          <button type="button" onClick={() => setValue('8')}>8</button>
+          <button type="button" onClick={() => setValue('9')}>9</button>
+          <button type="button" onClick={() => setValue('4')}>4</button>
+          <button type="button" onClick={() => setValue('5')}>5</button>
+          <button type="button" onClick={() => setValue('6')}>6</button>
+          <button type="button" onClick={() => setValue('1')}>1</button>
+          <button type="button" onClick={() => setValue('2')}>2</button>
+          <button type="button" onClick={() => setValue('3')}>3</button>
+        </div>
+        <div className="zeroDiv">
+          <button type="button" onClick={() => setValue('0')} className="zero">0</button>
+          <button type="button" onClick={() => setValue('.')}>.</button>
+        </div>
       </div>
-      <div className="zeroDiv">
-        <button type="button" onClick={() => setValue('0')} className="zero">0</button>
-        <button type="button" onClick={() => setValue('.')}>.</button>
-      </div>
-    </div>
+
+      <DisplayApi />
+    </>
   );
 };
 
